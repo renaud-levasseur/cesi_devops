8672afe9bb1ebd2476ce4180a7863ea457daa280
Florian <florian@ROGER>
2024-06-04 07:15:02 +0200

    fix await opqc

--- test/user_route.test.js
+++ test/user_route.test.js
@@ -1,81 +1,80 @@
 /*** IMPORT */
 const request = require('supertest')
+const DB = require('../api/db.config')
 const app = require('../api/app')
-const db = require('../api/db.config')
 
-let userId
+let userID
 
 describe('USER ROUTE', () => {
 
     afterAll( async () => {
-        await db.sequelize.close()
+        await DB.sequelize.close()
     })
 
-    // describe('TRY PUT', () => {
-    //     it('Should return 400 /=> Missing data', async () => {
-    //         const response = request(app)
-    //             .put('/users')
-    //             .send({
-    //                 pseudo: 'marcel',
-    //                 // email: 'marcel@roger.com',
-    //                 password: 'roger'
-    //             })
-    //         console.log(response.body)
-    //         expect(response.status).toBe(400)
-    //     })
+    describe('TRY PUT', () => {
+        it('Should return 400 /=> missing data', async () => {
+            const response = await request(app)
+                .put('/users')
+                .send({
+                    pseudo: 'marcel',
+                    //email: 'marcel@roger.com',
+                    password: 'roger'
+                })
+            expect(response.status).toBe(400)
+        })
 
-    //     it('Should return 201 /=> New User', async () => {
-    //         const response = request(app)
-    //             .put('/users')
-    //             .send({
-    //                 pseudo: 'marcel',
-    //                 email: 'marcel@roger.com',
-    //                 password: 'roger'
-    //             })
-    //         expect(response.status).toBe(201)
-    //         userId = response.body.data.id
-    //     })
-    // })
+        it('Should return 201 /=> new User', async () => {
+            const response = await request(app)
+                .put('/users')
+                .send({
+                    pseudo: 'marcel',
+                    email: 'marcel@roger.com',
+                    password: 'roger'
+                })
+            expect(response.status).toBe(201)
+            userID = response.body.data.id
+        })
+    })
 
     describe('TRY GET', () => {
-        it('Should return 200 /=> Get All Users', async () => {
-            const response = request(app).get('/users')
-            console.log(response)
+        it('Sould return 200 /=> Get All Users', async () => {
+            const response = await request(app).get(`/users`)
             expect(response.status).toBe(200)
         })
-        it('Should return 200 /=> Get One Users', async () => {
-            const response = request(app).get('/users/'+userId)
+        it('Should return 200 /=> Get user', async () => {
+            const response = await request(app).get(`/users/${userID}`)
             expect(response.status).toBe(200)
         })
-        it('Should return 404 /=> Get Bad Users', async () => {
-            const response = request(app).get('/users/11111111111111111111111111111')
+        it('Should return 404 /=> Get bad user', async () => {
+            const response = await request(app).get(`/users/11111111111111111111`)
             expect(response.status).toBe(404)
         })
     })
 
-    // describe('TRY PATCH', () => {
-    //     it('Should return 409 /=> Modify Bad User', async () => {
-    //         const response = request(app)
-    //             .patch('/users/11111111111111111111111111111111111111')
-    //             .send({
-    //                 pseudo: 'roger'
-    //             })
-    //         expect(response.status).toBe(409)
-    //     })
-    //     it('Should return 200 /=> Modify User', async () => {
-    //         const response = request(app)
-    //             .patch('/users/'+userId)
-    //             .send({
-    //                 pseudo: 'roger'
-    //             })
-    //         expect(response.status).toBe(200)
-    //     })
-    // })
+    describe('TRY PATCH', () => {
+        it("Sould return 409 /=> Modify bad user", async () => {
+            const response = await request(app)
+                .patch('/users/111111111111111111111')
+                .send({
+                    pseudo: 'roger'
+                })
+            expect(response.status).toBe(409)
+        })
+        it("Sould return 200 /=> Modify user", async () => {
+            const response = await request(app)
+                .patch(`/users/${userID}`)
+                .send({
+                    pseudo: 'roger'
+                })
+            expect(response.status).toBe(200)
+        })
+    })
 
-    // describe('TRY DELETE', () => {
-    //     it('Le bouzin doit dire ça 204', async () => {
-    //         const response = request(app).delete('/users/'+userId)
-    //         expect(response.status).toBe(204)
-    //     })
-    // })
+    describe('TRY DELETE', () => {
+        it('Sould return 204', async () => {
+            const response = await request(app)
+                .delete(`/users/${userID}`)
+            expect(response.status).toBe(204)
+        })
+    })
 })
